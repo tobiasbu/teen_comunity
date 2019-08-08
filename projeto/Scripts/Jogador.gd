62e39672347a1bc0dd14cf2896c78d5d5129ae56
tobiasbu <flamenco.bluegrass@gmail.com>
2019-08-08 15:28:36 -0300

    Preparando controle de personagem com analógico para mobile;

--- projeto/Scripts/Jogador.gd
+++ projeto/Scripts/Jogador.gd
@@ -4,8 +4,6 @@ extends KinematicBody
 const Declaration = preload("res://Personagem/CharacterDeclaration.gd");
 
 
-
-#onready var GRAVIDADE = -9.8
 onready var speed = 10
 onready var sideSpeed = 5
 onready var personagem = get_child(2)
@@ -15,26 +13,16 @@ onready var origin = orb.get_child(0);
 var onground = false;
 var sensibilidade = 0.005;
 var analog_velocity = Vector3();
-var mobile = true;
+var MOBILE = OS.get_name() == "Android";
 export var active = false;
 
 func _ready():
-	# Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	set_physics_process(true)
 	set_process_input(true)
-	if (mobile):
+	if (MOBILE):
 		speed *= 0.4
 
-#func _gravidade(delta):
-#	if GRAVIDADE > -9.8:
-#		GRAVIDADE -= 17.8 * delta
-#	else:
-#		GRAVIDADE = -9.8
-#
-#	move_and_slide(Vector3(0, GRAVIDADE * 50 * delta, 0), Vector3(0, -1, 0), 25.0)
-
 func setCamera(camera):
-	#origin.add_child(camera);
 	orb.camera = camera;
 	
 func setActive(flag):
@@ -42,8 +30,6 @@ func setActive(flag):
 	orb.active = flag;
 	origin.get_child(0).visible = true;
 	origin.get_child(0).current = true;
-	
-	# Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func getAnimState(up, left, right, down):
 	
@@ -65,10 +51,9 @@ func _move(delta):
 	var shift;
 	
 
-	if (!mobile):
-		
+	if (!MOBILE):	
 		up =  Input.is_action_pressed("ui_up")
-		shift = Input.is_action_pressed("ui_shift")
+		shift = Input.is_action_pressed("ui_shift") # TODO: correr segurando shift
 		down = Input.is_action_pressed("ui_down")
 		right = Input.is_action_pressed("ui_right")
 		left = Input.is_action_pressed("ui_left")
@@ -87,23 +72,12 @@ func _move(delta):
 		if down:
 			move_and_collide(transform.basis.z * speed * delta)
 	else:
-	
+		# TODO: rotação da visão da câmera baseado no analógico direito
 		var spd_norm = analog_velocity * speed * delta * -1;
-		
-		
-		
-		
-		#rotation.x = clamp(rotation.x - event.relative.y * sensibilidade, deg2rad(-45), deg2rad(45))
-		
-		
 		move_and_collide(spd_norm);
-		
-		
-		
 		#var rad = atan2(analog_velocity.y, analog_velocity.x);
 		#spd.z = -spd_norm.x * velocidade * delta;
 		#spd.x = spd_norm.y * velocidade * delta;
-		
 		if (analog_velocity.length() > 0):
 			#var rad = atan2(analog_velocity.x, analog_velocity.z);
 			#rotation.y = rad;
@@ -112,22 +86,10 @@ func _move(delta):
 			right = true;
 			down = false;
 			
-	
-	
 	#if (spd.x != 0 || spd.z != 0):
 	#var rad = atan2(analog_velocity.y, analog_velocity.x)
 	#move_and_collide(transform.basis.z * rad * velocidade * 1 * delta);
-	
-	
-	
-	
-	# if right:
-		
-	# if left:
-	# 	move_and_collide(transform.basis.x * hdir * -1 * delta)
-	#if space:
-	#	GRAVIDADE = 10
-	
+
 	# personagem._olhaPara(orb.get_transform().inverse())
 	personagem._update(getAnimState(up, left, right, down))
 
@@ -135,8 +97,7 @@ func _physics_process(delta):
 	if (active):
 		_move(delta)
 
-func _input(event):
-	
+func _input(event):	
 	if (active):
 		#if event is InputEventMouseMotion:
 			#rotation.y -= event.relative.x * sensibilidade
